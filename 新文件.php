74920b6d3478f919709e0adcb894bccc0ef6cb17
bybxn <119147256+bybxn@users.noreply.github.com>
2022-11-27 12:15:42 +0800

    Add files via upload

---
+++ 新文件.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>哈喽，你好(ㅇㅅㅇ❀)
+    </title>
+  <body bgcolor="ffefec">
+    <img src="https://i.328888.xyz/img/2022/11/26/yCob.jpeg" alt="logo" width="100px" height="100px">
+    <h1></h1>
+    <font color="#87CEFA" size="6">欢迎访问</font>
+    <h1></h1>
+    <font color="#87CEFA">你是第一个访问我的网站的人</font>
+    <h1></h1>
+    <font color="#87cefa">但绝不会是最后一个</font>
+    <h1></h1>
+    <font color="#87cefa">希望下次我的网站你还能看见(ㅇㅅㅇ❀)</font>
+    <h1></h1>
+    <font color="#87cefa">那。。。下次见？(*^ω^*)</font>
+  </body>
+</html>
